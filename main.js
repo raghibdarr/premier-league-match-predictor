ec09b472bade24ffa8d5b5f4ec5c8d8dccc67906
raghibdarr <raghibdarr@gmail.com>
2021-10-28 17:53:07 +0100

    added generateMatch function to premData object

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const generateResult = (homeTeamPI, awayTeamPI) => {
     const morePointsWonAtHome = 0.39; // teams win 0.39 more points at home than away
     const availablePoints = 3;
     const homeAdvantage = morePointsWonAtHome/availablePoints + 1;
-    const eloDifference = homeAdvantage * homeTeamPI - awayTeamPI;
+    const eloDifference = (homeAdvantage * homeTeamPI) - awayTeamPI;
+    console.log(eloDifference.toFixed(2));
+
     if (Math.abs(eloDifference) <= 3) {
         console.log("draw");
         return "DRAW";
@@ -77,6 +79,48 @@ const premData = {
             }
         }
         return this.potentialScores;
+    },
+    generateMatch(homeTeamName, awayTeamName) {
+        let homeTeam = this.teams[homeTeamName];
+        let awayTeam = this.teams[awayTeamName];
+        let result = generateResult(homeTeam, awayTeam);
+        const randomNum = generateRandomNumber(100);
+        let finalResult;
+
+        switch (result) {
+            case "WIN":
+                if (randomNum <= 85) {
+                    const likelyWinningArray = this.potentialScores.winningScores.likelyWinningScores;
+                    finalResult = likelyWinningArray[generateRandomNumber(likelyWinningArray.length)];
+                } else {
+                    const unlikelyWinningArray = this.potentialScores.winningScores.unlikelyWinningScores;
+                    finalResult = unlikelyWinningArray[generateRandomNumber(unlikelyWinningArray.length)];
+                }
+                break;
+            case "LOSS":
+                if (randomNum <= 85) {
+                    const likelyLosingArray = this.potentialScores.losingScores.likelyLosingScores;
+                    finalResult = likelyLosingArray[generateRandomNumber(likelyLosingArray.length)];
+                } else {
+                    const unlikelyLosingArray = this.potentialScores.losingScores.unlikelyLosingScores;
+                    finalResult = unlikelyLosingArray[generateRandomNumber(unlikelyLosingArray.length)];
+                }
+                break;
+            case "DRAW":
+                if (randomNum <= 85) {
+                    const likelyDrawingArray = this.potentialScores.drawingScores.likelyDrawingScores;
+                    finalResult = likelyDrawingArray[generateRandomNumber(likelyDrawingArray.length)];
+                } else {
+                    const unlikelyDrawingArray = this.potentialScores.drawingScores.unlikelyDrawingScores;
+                    finalResult = unlikelyDrawingArray[generateRandomNumber(unlikelyDrawingArray.length)];
+                }
+                break;
+        }
+
+        let toString = `Result: ${homeTeamName} ${finalResult} ${awayTeamName}`;
+        // console.log(finalResult);
+        return(toString);
+
     }
 
 
@@ -89,3 +133,11 @@ const data = premData;
 console.log(data.generatePotentialScores());
 // console.log(data.teams['arsenal']);
 console.log(data.generateMatch('arsenal', 'brighton'));
+console.log(data.generateMatch('brighton', 'arsenal'));
+console.log(data.generateMatch('man city', 'norwich'));
+console.log(data.generateMatch('norwich', 'man city'));
+console.log(data.generateMatch('spurs', 'chelsea'));
+console.log(data.generateMatch('chelsea', 'spurs'));
+console.log(data.generateMatch('everton', 'leicester'));
+console.log(data.generateMatch('leicester', 'everton'));
+
